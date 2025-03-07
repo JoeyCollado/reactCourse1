827d5896d71b908d42b5dbab2cb9d6c338e2df4a
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-03-07 19:20:30 +0800

    incrementer

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { UserDetails } from "./components/userDetails";
 export default function App() {
   const [username,setUsername] = useState("");
   const [email,setEmail] = useState("");
+  const [counter, setCounter] = useState(3);
   const [users, setUsers] = useState([
     {
       id: 1,
@@ -23,7 +24,15 @@ export default function App() {
   ]);
   return (
     <div className="bg-slate-600 h-screen">
-      <form>
+      <form onSubmit={e => {
+        e.preventDefault()
+        const newUser = {
+          id, counter,
+          username,
+          email,
+        };
+        setCounter((currentCounter) => currentCounter + 1)
+      }}>
       <div>
       <label htmlFor="username">Username</label>
       <input name="username" id="username" value={username} onChange={(e) => {
