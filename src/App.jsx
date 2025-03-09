5ce0d087e12a696a7df78de6e259faf68f4b3840
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-03-09 14:45:30 +0800

    div and button

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,16 @@ import React from 'react'
 import { useState, useEffect } from 'react'
 
 const App = () => {
-  const = [counter, setCounter] = useState(0)
+  const [counter, setCounter] = useState(0)
 
   useEffect(() => {
     document.title = "React Tutorial"
   });
-  
+
   return (
     <div>
-      
+      <div>You Clicked the button {counter} times</div>
+      <button onClick={() => setCounter((count) => count + 1)}>Click Me</button>
     </div>
   )
 }
