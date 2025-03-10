967c95b4ba82964141d3e2b92f01b755d686d2a4
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-03-10 15:51:52 +0800

    dependency def

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
 import { useState, useEffect } from 'react'
 
 const App = () => {
   const [counter, setCounter] = useState(0)
 
   useEffect(() => {
-    document.title = "React Tutorial"
-  });
+    document.title = `React Tutorial  ${counter}`
+  }, []); //call this callback function everytime the useState is updated by default with no dependencies array
+
+//empty dependency = callback func only going to be invoke one time only even when we update the state of component
+//this is useful when we for example want to fetch our user in api and we want to render it to our document and you dont want to refetch the user every single time
+
+//without dependency = it's going to be invoke every time your component state is updated
+
+//with dependency
 
   return (
     <div>
