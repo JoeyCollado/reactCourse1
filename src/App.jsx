6db42c953c94147b84e9bf9dd0b5960a171717e2
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-03-11 20:34:37 +0800

    save point

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,24 @@ import { useState, useEffect } from 'react'
 
 const App = () => {
   const [counter, setCounter] = useState(0)
-  const [sync, setSync] = useState(false)
+  //const [sync, setSync] = useState(false)
+  //promises = have states, pending, fulfilled, and rejected states, important in fetching api's
 
   useEffect(() => {
     document.title = `React Tutorial  ${counter}`
-  }, [sync]); //call this callback function everytime the useState is updated by default with no dependencies array
+  }, [counter]); //call this callback function everytime the useState is updated by default with no dependencies array
 
-//empty dependency = callback func only going to be invoke one time only even when we update the state of component
-//this is useful when we for example want to fetch our user in api and we want to render it to our document and you dont want to refetch the user every single time
+  useEffect(() => { //fetch data (url, {options for requested data})
+    fetch('https://jsonplaceholder.typicode.com/users', {
+      method: 'GET',
+      // body: {
+      //  // username: ''
+      // }
+    }).then((response) => { //getting the response
+      console.log(response)
+    })
+  })
 
-//without dependency = it's going to be invoke every time your component state is updated
-
-//with dependency
-//[counter] = only invoke when counter changes as a dependency
   return (
     <div>
       <div>You Clicked the button {counter} times</div>
